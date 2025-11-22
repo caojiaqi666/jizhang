14f6e4c4300bcb97e539fffbfeb91577cee4ebb9
曹佳琦1 <1508771379@qq.com>
2025-11-22 21:00:49 +0800

    feat: build

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const withPWA = require("next-pwa")({
 });
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   images: {
     remotePatterns: [
